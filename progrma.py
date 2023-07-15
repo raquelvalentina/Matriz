1006e84d5ef83adf424a36b784aca54e7e32f236
= <=>
2023-07-14 20:44:05 -0500

    ultimo

--- progrma.py
+++ progrma.py
@@ -1,49 +1,28 @@
 import numpy as np
 
-# Paso 1: Leer el archivo de datos
+def detectar_listas_en_archivo(nombre_archivo):
+    vector_de_observaciones = []
+    matriz_diseno = []
+
+    with open(nombre_archivo, 'r') as archivo:
+        contenido = archivo.read()
+        lineas = contenido.split('\n')
+
+        for linea in lineas:
+            if 'vector de observaciones' in linea:
+                elementos = linea.split('=')[1].strip()[1:-1].split(', ')
+                vector_de_observaciones = [elemento.strip("'") for elemento in elementos]
+            elif 'matriz_diseno' in linea:
+                elementos = linea.split('=')[1].strip()[2:].split('], [')
+                matriz_diseno = [elemento.strip("[]'").split("', '") for elemento in elementos]
+
+    return vector_de_observaciones, matriz_diseno
+
 nombre_archivo = input("Ingrese el nombre del archivo: ")
-datos = np.load(nombre_archivo)
 
-# Paso 2: Identificar la matriz de diseño
-matriz_diseno = datos['matriz_diseno']
+vector_observaciones, matriz_diseno = detectar_listas_en_archivo(nombre_archivo)
 
-# Paso 3: Identificar el vector de observaciones
-vector_observaciones = datos['vector_observaciones']
-
-# Paso 4: Calcular el vector solución
-vector_solucion = np.linalg.lstsq(matriz_diseno, vector_observaciones, rcond=None)[0]
-
-# Paso 5: Calcular el vector de errores residuales
-vector_errores_residuales = vector_observaciones - np.dot(matriz_diseno, vector_solucion)
-
-# Paso 6: Calcular la precisión del ajuste
-precision_ajuste = np.linalg.norm(vector_errores_residuales)
-
-# Paso 7: Calcular la matriz de varianzas y covarianzas
-matriz_varianzas_covarianzas = np.linalg.inv(np.dot(matriz_diseno.T, matriz_diseno))
-
-# Paso 8: Imprimir el archivo
-print("Archivo de datos:")
-print(datos)
-
-# Paso 9: Imprimir los resultados del proceso
 print("Matriz de diseño:")
 print(matriz_diseno)
 print("Vector de observaciones:")
 print(vector_observaciones)
-
-# Paso 10: Crear un archivo llamado resultados
-archivo_resultados = open("resultados.txt", "w")
-
-# Paso 11: Agregar los resultados al archivo resultados
-archivo_resultados.write("Vector solución:\n")
-archivo_resultados.write(str(vector_solucion) + "\n")
-archivo_resultados.write("Vector de errores residuales:\n")
-archivo_resultados.write(str(vector_errores_residuales) + "\n")
-archivo_resultados.write("Precisión del ajuste:\n")
-archivo_resultados.write(str(precision_ajuste) + "\n")
-archivo_resultados.write("Matriz de varianzas y covarianzas:\n")
-archivo_resultados.write(str(matriz_varianzas_covarianzas) + "\n")
-
-archivo_resultados.close()
-'''Este código lee el nombre del archivo de datos proporcionado por el usuario, calcula el vector solución, el vector de errores residuales, la precisión del ajuste y la matriz de varianzas y covarianzas. Luego, imprime el archivo de datos y los resultados del proceso. Finalmente, crea un archivo llamado "resultados.txt" y agrega los resultados al archivo.'''
