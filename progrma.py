c4175347fab343521ffd60f585d247ad2486242b
ʀʀ <66224544+raquelvalentina@users.noreply.github.com>
2023-07-18 07:32:06 -0500

    Update progrma.py
    ppp

--- progrma.py
+++ progrma.py
@@ -1,94 +1,38 @@
 import numpy as np
 
-def detectar_listas_en_archivo(nombre_archivo):
-    with open(nombre_archivo, 'r') as archivo:
-        contenido = archivo.read()
-        lineas = contenido.split('\n')
-        index = len(lineas)
-        matriz_diseno = []
-        vector_de_observaciones = []
-        vector_de_paramentrosX=[]
+def calcular_valores(vector_de_observaciones, matriz_diseno, A, X, e, sigma_o2):
+    # Cálculo de la matriz A
+    A = matriz_diseno.T
 
-        for var in range(index):
-            if 'vector de observaciones' in lineas[var]:
-                elementos = lineas[var].split('=')[1].strip()[1:-1].split(', ')
-                vector_de_observaciones = [elemento.strip("'") for elemento in elementos]
-            elif 'matriz_diseno' in lineas[var] or '[]' in lineas[var]:
-                elementos = lineas[var].split('=')[1].strip()[1:-1].split('], [')
-                matriz_diseno = [elemento.strip("[]'").split("', '") for elemento in elementos]
+    # Cálculo de la matriz (A^T * P * A)^-1
+    ATA_inv = np.linalg.inv(np.dot(np.dot(A.T, sigma_o2), A))
 
-            elif 'vector de parametros desconocidos' in lineas[var]:
-                elementos = lineas[var].split('=')[1].strip()[1:-1].split('], [')
-                vector_de_paramentrosX = [elemento.strip("[]'").split("', '") for elemento in elementos]
+    # Cálculo de X_hat
+    X_hat = np.dot(ATA_inv, np.dot(A.T, vector_de_observaciones))
 
+    # Cálculo del vector de residuos e_hat
+    e_hat = vector_de_observaciones - np.dot(A, X_hat)
 
-    return vector_de_observaciones, matriz_diseno, vector_de_paramentrosX
+    # Cálculo de sigma_hat^2
+    sigma_hat_squared = np.dot(np.dot(e_hat.T, sigma_o2), e_hat) / (len(vector_de_observaciones) - len(X_hat))
 
-nombre_archivo1= ("archivo.txt")
+    # Cálculo de MATRIZ_DE_VARIANZA_COVARIANZA
+    MATRIZ_DE_VARIANZA_COVARIANZA = ATA_inv * sigma_hat_squared
 
-vector_observaciones1, matriz_diseno1, vector_de_paramentrosX1 = detectar_listas_en_archivo(nombre_archivo1)
+    return X_hat, e_hat, sigma_hat_squared, MATRIZ_DE_VARIANZA_COVARIANZA
 
+# Datos proporcionados
+vector_de_observaciones = np.array([103.758, 104.113, 105.713, 107.313, 108.913, 110.513, 112.113])
+matriz_diseno = np.array([[1695.557, 700.000, 720.000, 740.000, 760.000, 780.000, 800.000], [1, 1, 1, 1, 1, 1, 1]])
+A = np.array([[0.000104815, -0.07779615], [-0.07779615, 57.884906]])
+X = np.array([0.079997848, 48.11466023])
+e = np.array([0.0003, -0.0002, -0.0001, -0.0001, 0.0000, 0.0000, 0.0001])
+sigma_o2 = np.array([[2.43507E-08, 2.55232E-12, -1.8944E-09], [0, 1.8944E-09, 1.8944E-09]])
 
-def gauss_markov(y, A):
-    n = len(y)
-    m = len(A[0])
-    
-    # Crear matriz de diseño A
-    A = np.array(A)
-    
-    # Crear vector de observaciones y
-    y = np.array(y)
-    
-    # Calcular la matriz de pesos P
-    P = np.eye(n)
-    
-    # Calcular la matriz de coeficientes A transpuesta
-    A_transpose = np.transpose(A)
-    
-    # Calcular el producto A transpuesta * P * A
-    product = np.dot(A_transpose, np.dot(P, A))
-    
-    # Calcular la inversa de A transpuesta * P * A
-    inverse = np.linalg.inv(product)
-    
-    # Calcular el producto A transpuesta * P * y
-    product2 = np.dot(A_transpose, np.dot(P, y))
-    
-    # Calcular el vector solución X
-    X_hat = np.dot(inverse, product2)
-    
-    # Calcular el vector de errores residuales e
-    e_hat = np.dot(A, X_hat) - y
-    
-    # Calcular la precisión del ajuste σ^2
-    sigma_hat_squared = np.dot(np.transpose(e_hat), np.dot(P, e_hat)) / (n - m)
-    
-    # Calcular la matriz de varianzas y covarianzas ΣXX
-    Sigma_XX = sigma_hat_squared * inverse
-    
-    return X_hat, e_hat, sigma_hat_squared, Sigma_XX
+# Cálculo de los valores
+X_hat, e_hat, sigma_hat_squared, MATRIZ_DE_VARIANZA_COVARIANZA = calcular_valores(vector_de_observaciones, matriz_diseno, A, X, e, sigma_o2)
 
-# Ejemplo de uso
-# y = vector_observaciones1
-# A = matriz_diseno1
-
-# X_hat, e_hat, sigma_hat_squared, Sigma_XX = gauss_markov(y, A)
-
-# print("Vector solución X_hat:")
-# print(X_hat)
-
-# print("Vector de errores residuales e_hat:")
-# print(e_hat)
-
-# print("Precisión del ajuste sigma_hat_squared:")
-# print(sigma_hat_squared)
-
-# print("Matriz de varianzas y covarianzas Sigma_XX:")
-# print(Sigma_XX)
-
-
-
-# print("Matriz de diseño:")
-# print(matriz_diseno1)
-# print("Vector de observaciones:")
-# print(vector_observaciones1)
+print("X_hat:", X_hat)
+print("VECTOR_ERRORES_RESIDUALES:", e_hat)
+print("PRECISION_DE_AJUSTE:", sigma_hat_squared)
+print("MATRIZ_DE_VARIANZA_COVARIANZA:", MATRIZ_DE_VARIANZA_COVARIANZA)
